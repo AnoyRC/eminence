57e9753c060ff0c544e7be1d3d84b0e52ee5c3ee
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2023-09-22 22:15:08 +0530

    Main Page and Launch App Fix

--- components/ui/Button.js
+++ components/ui/Button.js
@@ -1,24 +1,18 @@
-'use client';
+"use client";
 
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 
-const Button = ({ label, rounded, fullWidth, color, style, location }) => {
-  const router = useRouter();
-
-  const handleClick = () => {
-    router.push(location);
-  };
-
+const Button = ({ label, rounded, fullWidth, color, style, onClick }) => {
   return (
     <button
       className={
-        (rounded ? 'rounded-full ' : '') +
-        (style + ' ' || '') +
-        (fullWidth ? 'w-full ' : 'w-fit ') +
+        (rounded ? "rounded-full " : "") +
+        (style + " " || "") +
+        (fullWidth ? "w-full " : "w-fit ") +
         color +
-        ' hover:scale-105 transition-all duration-300 ease-in-out active:scale-100'
+        " hover:scale-105 transition-all duration-300 ease-in-out active:scale-100"
       }
-      onClick={handleClick}
+      onClick={onClick}
     >
       {label}
     </button>
