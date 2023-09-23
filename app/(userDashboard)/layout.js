42f578d95c1e314d0d84406947bc912838af7988
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2023-09-24 02:15:33 +0530

    Sidebar Design Added with minor layout fixes

--- app/(userDashboard)/layout.js
+++ app/(userDashboard)/layout.js
@@ -1,16 +1,16 @@
-import DashboardContainer from '@/components/layout/dashboard/DashboardContainer';
-import PageHeader from '@/components/layout/dashboard/PageHeader';
-import Sidebar from '@/components/layout/dashboard/Sidebar';
+import DashboardContainer from "@/components/layout/dashboard/DashboardContainer";
+import PageHeader from "@/components/layout/dashboard/PageHeader";
+import Sidebar from "@/components/layout/dashboard/Sidebar";
 
 export default function DashLayout({ children }) {
   return (
-    <>
+    <div className="p-[12px] bg-black h-screen w-screen overflow-x-hidden flex">
       <Sidebar />
 
       <DashboardContainer>
         <PageHeader />
         {children}
       </DashboardContainer>
-    </>
+    </div>
   );
 }
