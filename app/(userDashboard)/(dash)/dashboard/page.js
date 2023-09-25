10bb332faaf211f31942e99d362e2139a3a4b44b
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2023-09-25 20:44:12 +0530

    Graph with Live Updates added

--- app/(userDashboard)/(dash)/dashboard/page.js
+++ app/(userDashboard)/(dash)/dashboard/page.js
@@ -1,13 +1,12 @@
-import MyWallet from '@/components/layout/dashboard/home/MyWallet';
-import Contacts from '@/components/layout/dashboard/home/Contacts';
-import Transactions from '@/components/layout/dashboard/home/Transactions';
-import Graph from '@/components/layout/dashboard/home/Graph';
+import MyWallet from "@/components/layout/dashboard/home/MyWallet";
+import Contacts from "@/components/layout/dashboard/home/Contacts";
+import Transactions from "@/components/layout/dashboard/home/Transactions";
+import Graph from "@/components/layout/dashboard/home/Graph";
 
 export default function Home() {
   return (
-    <section>
-      {/* Import My Wallet, Contacts, Transactions, Graph */}
-      Dashboard
+    <section className="w-full h-full flex flex-col">
+      <Graph />
     </section>
   );
 }
--- app/(userDashboard)/(dash)/layout.js
+++ app/(userDashboard)/(dash)/layout.js
@@ -3,7 +3,7 @@ import AsideContainer from "@/components/layout/dashboard/home/AsideContainer";
 export default function DashLayout({ children }) {
   return (
     <main className="flex justify-between w-full flex-grow">
-      {children}
+      <div className="flex-grow px-[16px] py-[20px]">{children}</div>
       <AsideContainer />
     </main>
   );
