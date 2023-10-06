861a1d666c4f70c0d29304b43d9a61db67cacb82
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2023-10-07 01:38:31 +0530

    Bug fixes + Resolved Conflict from Merge

--- components/layout/myContacts/Contacts.js
+++ components/layout/myContacts/Contacts.js
@@ -1,17 +1,18 @@
-import ContactList from './ContactList';
+import ContactList from "./ContactList";
+import ContactTab from "./ContactTab";
 
 const Contacts = () => {
   const contacts = [
-    { name: 'Sourabh' },
-    { name: 'Gautam Raj' },
-    { name: 'Anoy' },
-    { name: 'Pratik' },
+    { name: "Sourabh" },
+    { name: "Gautam Raj" },
+    { name: "Anoy" },
+    { name: "Pratik" },
   ];
 
   return (
-    <section className="flex flex-col gap-3 w-full">
+    <section className="flex flex-col gap-3 w-full mt-6">
       {contacts.map((contact) => (
-        <ContactList key={contact.name} name={contact.name} />
+        <ContactTab key={contact.name} name={contact.name} />
       ))}
     </section>
   );
