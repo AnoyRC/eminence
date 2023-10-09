5fa534d71cf9a348b52a17ceb3399ba7be68740a
Gautam Raj <ashragautam25@gmail.com>
2023-10-09 10:14:14 +0530

    Update voucher route

---
+++ components/layout/voucher/VoucherReceive.js
@@ -0,0 +1,37 @@
+'use client';
+
+import { Input } from '@material-tailwind/react';
+
+import Balance from '@/components/ui/Balance';
+import Button from '@/components/ui/Button';
+
+export default function VoucherReceive({ currency, balance, value }) {
+  return (
+    <div className="w-full items-center rounded-[8px] p-[1px] bg-gradient-priamry">
+      <div className="flex justify-between h-full w-full bg-black rounded-lg p-2 gap-3 items-center">
+        <div className="w-2/5">
+          <Balance
+            symbol={currency}
+            balance={balance}
+            value={value}
+            type="Receive"
+            editable
+            onChange={(e) => console.log(e.target.value)}
+          />
+        </div>
+
+        <form className="flex-grow px-5 flex flex-col gap-4">
+          <Input variant="standard" label="Enter Password" color="white" />
+          <Input variant="standard" label="Receiver Addresss" color="white" />
+
+          <Button
+            label="Claim"
+            fullWidth
+            color="bg-gradient-priamry"
+            style="font-bold py-2 rounded-lg mt-5"
+          />
+        </form>
+      </div>
+    </div>
+  );
+}
