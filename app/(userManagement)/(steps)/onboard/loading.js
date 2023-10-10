3cff95045942b811402bfb0717b59f251a128d1f
Gautam Raj <ashragautam25@gmail.com>
2023-10-10 23:36:37 +0530

    Add loading state

---
+++ app/(userManagement)/(steps)/onboard/loading.js
@@ -0,0 +1,6 @@
+import LoadingAnimation from '@/components/LoadingAnimation';
+
+const loading = () => {
+  return <LoadingAnimation width={'w-full'} height={'h-full'} />;
+};
+export default loading;
