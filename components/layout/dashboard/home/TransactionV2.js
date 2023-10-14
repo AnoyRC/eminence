87565bbdd74eb5c77e0bd1f23db76a8e2a78da7f
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2023-10-14 16:45:22 +0530

    Failsafe for no transaction

--- components/layout/dashboard/home/TransactionV2.js
+++ components/layout/dashboard/home/TransactionV2.js
@@ -20,6 +20,7 @@ import TransactionTab from "./TransactionTab";
 import { ArrowPathIcon } from "@heroicons/react/24/solid";
 import useTransaction from "@/hooks/useTransaction";
 import { removeAllTransactions } from "@/redux/profileSlice";
+import Image from "next/image";
 
 const TABLE_HEAD = ["Name", "Amount", "Date", "Type", "TxID"];
 
@@ -47,7 +48,7 @@ export default function TransactionV2({ limit = 5 }) {
           className="rounded-none bg-black"
         >
           <div className="flex items-center justify-between">
-            <div className=" flex flex-col justify-between gap-8 pb-1">
+            <div className=" flex flex-col justify-between gap-8 pb-3">
               <div>
                 <Typography
                   variant="h5"
@@ -81,50 +82,69 @@ export default function TransactionV2({ limit = 5 }) {
             </button>
           </div>
         </CardHeader>
-        <CardBody className="p-2 px-0">
-          <table className="w-full table-auto text-left">
-            <thead>
-              <tr>
-                {TABLE_HEAD.map((head, index) => (
-                  <th
-                    key={head}
-                    className="border-y border-white bg-[#1C1D22] p-4 hover:bg-[#1C1D22]/50 cursor-pointer"
-                  >
-                    <Typography
-                      variant="small"
-                      color="white"
-                      className={
-                        "flex items-center justify-between font-normal leading-none opacity-70 " +
-                        myFont.className
-                      }
+
+        {transactions.length === 0 && (
+          <div className="flex items-center justify-center rounded-[8px] mt-2">
+            <div className="w-full h-[300px] rounded-[8px] bg-[#1c1d22] z-10 flex flex-col justify-center items-center">
+              <Image src="/images/logo.png" alt="logo" width={50} height={50} />
+
+              <h3 className="text-primary-white font-bold text-2xl leading-normal">
+                Eminence
+              </h3>
+
+              <p className="text-primary-white max-w-xs text-center">
+                No transactions to show
+              </p>
+            </div>
+          </div>
+        )}
+
+        {transactions.length > 0 && (
+          <CardBody className="p-2 px-0">
+            <table className="w-full table-auto text-left">
+              <thead>
+                <tr>
+                  {TABLE_HEAD.map((head, index) => (
+                    <th
+                      key={head}
+                      className="border-y border-white bg-[#1C1D22] p-4 hover:bg-[#1C1D22]/50 cursor-pointer"
                     >
-                      {head}
-                      {index !== TABLE_HEAD.length - 1 && (
-                        <ChevronUpDownIcon
-                          strokeWidth={2}
-                          className="h-4 w-4"
-                        />
-                      )}
-                    </Typography>
-                  </th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {transactions.length > 0 &&
-                transactions.slice(0, limit).map((transaction, index) => {
-                  return (
-                    <TransactionTab
-                      key={transaction.txId}
-                      transaction={transaction}
-                      length={transactions.length}
-                      index={index}
-                    />
-                  );
-                })}
-            </tbody>
-          </table>
-        </CardBody>
+                      <Typography
+                        variant="small"
+                        color="white"
+                        className={
+                          "flex items-center justify-between font-normal leading-none opacity-70 " +
+                          myFont.className
+                        }
+                      >
+                        {head}
+                        {index !== TABLE_HEAD.length - 1 && (
+                          <ChevronUpDownIcon
+                            strokeWidth={2}
+                            className="h-4 w-4"
+                          />
+                        )}
+                      </Typography>
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {transactions.length > 0 &&
+                  transactions.slice(0, limit).map((transaction, index) => {
+                    return (
+                      <TransactionTab
+                        key={transaction.txId}
+                        transaction={transaction}
+                        length={transactions.length}
+                        index={index}
+                      />
+                    );
+                  })}
+              </tbody>
+            </table>
+          </CardBody>
+        )}
       </Card>
     </div>
   );
