7cad4ed0a0b704e1d6973467604960c5a274d8c3
Gautam Raj <ashragautam25@gmail.com>
2023-11-01 03:44:26 +0530

    add ably dashoard channel

--- hooks/useLiveGraph.js
+++ hooks/useLiveGraph.js
@@ -1,32 +1,26 @@
-"use client";
-import { setHistory, setWeekStat } from "@/redux/graphSlice";
-import fetch from "cross-fetch";
-import { useState } from "react";
-import { useDispatch } from "react-redux";
+'use client';
+import { setHistory, setWeekStat } from '@/redux/graphSlice';
+import fetch from 'cross-fetch';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 export default function useLiveGraph() {
   const [wsHistoryClient, setWsHistoryClient] = useState(null);
   const dispatch = useDispatch();
 
   const FetchHistory = async (interval) => {
-    const price = await (
-      await fetch("https://api.binance.com/api/v3/ticker/price?symbol=SOLUSDT")
-    ).json();
-
-    console.log(price);
-
     if (wsHistoryClient) wsHistoryClient.close();
 
-    const client = new WebSocket("wss://ws-api.binance.com:443/ws-api/v3");
+    const client = new WebSocket('wss://ws-api.binance.com:443/ws-api/v3');
 
     client.onopen = function () {
-      console.log("WebSocket Client Connected");
+      console.log('WebSocket Client Connected');
       client.send(
         JSON.stringify({
-          id: "1dbbeb56-8eea-466a-8f6e-86bdcfa2fc0b",
-          method: "klines",
+          id: '1dbbeb56-8eea-466a-8f6e-86bdcfa2fc0b',
+          method: 'klines',
           params: {
-            symbol: "SOLUSDT",
+            symbol: 'SOLUSDT',
             interval: interval.toString(),
             limit: 25,
           },
@@ -35,7 +29,7 @@ export default function useLiveGraph() {
     };
 
     client.onmessage = function (e) {
-      if (typeof e.data === "string") {
+      if (typeof e.data === 'string') {
         const result = JSON.parse(e.data).result;
 
         const data = result.map((item) => {
@@ -43,9 +37,9 @@ export default function useLiveGraph() {
             ticker: Number(item[1]).toFixed(2),
             date:
               new Date(item[0]).getDate() +
-              "/" +
+              '/' +
               new Date(item[0]).getMonth() +
-              "/" +
+              '/' +
               new Date(item[0]).getFullYear(),
             time: new Date(item[0]).toLocaleTimeString(),
           };
@@ -56,10 +50,10 @@ export default function useLiveGraph() {
       setTimeout(function timeout() {
         client.send(
           JSON.stringify({
-            id: "1dbbeb56-8eea-466a-8f6e-86bdcfa2fc0b",
-            method: "klines",
+            id: '1dbbeb56-8eea-466a-8f6e-86bdcfa2fc0b',
+            method: 'klines',
             params: {
-              symbol: "SOLUSDT",
+              symbol: 'SOLUSDT',
               interval: interval.toString(),
               limit: 25,
             },
@@ -69,7 +63,7 @@ export default function useLiveGraph() {
     };
 
     client.onclose = function () {
-      console.log("WebSocket Client Closed");
+      console.log('WebSocket Client Closed');
     };
 
     setWsHistoryClient(client);
@@ -78,7 +72,7 @@ export default function useLiveGraph() {
   const FetchWeekHistory = async () => {
     const price = await (
       await fetch(
-        "https://api.binance.com/api/v3/klines?symbol=SOLUSDT&interval=1d&limit=7"
+        'https://api.binance.com/api/v3/klines?symbol=SOLUSDT&interval=1d&limit=7'
       )
     ).json();
 
